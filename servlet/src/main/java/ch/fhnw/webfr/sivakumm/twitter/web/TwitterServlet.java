2b50365891c1f6f29ab5d76d2c89ef98b8f91240
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-24 19:30:16 +0100

    adds dispatcher & detailed view of tweets

--- servlet/src/main/java/ch/fhnw/webfr/sivakumm/twitter/web/TwitterServlet.java
+++ servlet/src/main/java/ch/fhnw/webfr/sivakumm/twitter/web/TwitterServlet.java
@@ -3,6 +3,7 @@ package ch.fhnw.webfr.sivakumm.twitter.web;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
 
 import ch.fhnw.webfr.sivakumm.twitter.domain.Tweet;
 import ch.fhnw.webfr.sivakumm.twitter.persistence.TweetRepository;
@@ -27,6 +28,18 @@ public class TwitterServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchRequest(req, resp);
+    }
+
+    private void dispatchRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getRequestURI().matches(req.getContextPath() + "/\\d")) {
+            showDetailedTweetPage(req, resp);
+        } else {
+            showOverviewPage(req, resp);
+        }
+    }
+
+    private void showOverviewPage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         List<Tweet> tweets = tweetRepository.findAll();
 
         resp.setContentType("text/html");
@@ -39,6 +52,25 @@ public class TwitterServlet extends HttpServlet {
             }
             writer.append("</i></div>");
         }
+        writer.append("</body></html>");
+    }
+
+    private void showDetailedTweetPage(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String[] pathElements = req.getRequestURI().split("/");
+        Optional<Tweet> tweet = tweetRepository.findById(Integer.parseInt(pathElements[pathElements.length - 1]));
+
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+        writer.append("<html><head><title>Tweet</title></head><body>");
+
+        if (tweet.isEmpty()) {
+            writer.append("<h1>Tweet not found</h1>");
+        } else {
+            writer.append("<h3>" + tweet.get().getUsername() + " @ " + tweet.get().getDate() + "</h3>");
+            writer.append("<p>" + tweet.get().getTweet() + "</p>");
+            writer.append("<br /><a href='" + req.getContextPath() + "'>Back to all Tweets</a>");
+        }
+
         writer.append("</body></html>");
     }
 }
