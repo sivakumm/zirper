664073cb1e96b190762808d1c00a654323aa0ee8
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-01-24 22:53:24 +0100

    adds form & process to add zirps

--- servlet/src/main/java/ch/fhnw/webfr/sivakumm/zirper/web/ZirperServlet.java
+++ servlet/src/main/java/ch/fhnw/webfr/sivakumm/zirper/web/ZirperServlet.java
@@ -2,10 +2,13 @@ package ch.fhnw.webfr.sivakumm.zirper.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import ch.fhnw.webfr.sivakumm.zirper.domain.Zirp;
 import ch.fhnw.webfr.sivakumm.zirper.persistence.ZirpRepository;
@@ -28,14 +31,27 @@ public class ZirperServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        dispatchRequest(req, resp);
+        dispatchGetRequest(req, resp);
     }
 
-    private void dispatchRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        if (req.getRequestURI().matches(req.getContextPath() + "/web" + "/\\d+$")) {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        dispatchPostRequest(req, resp);
+    }
+
+    private void dispatchGetRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getRequestURI().matches(req.getContextPath() + "/web" + "/\\d+/?$")) {
             showDetailedTweetPage(req, resp);
-        } else if (req.getRequestURI().matches(req.getContextPath() + "/web")) {
+        } else if (req.getRequestURI().matches(req.getContextPath() + "/web/?")) {
             showOverviewPage(req, resp);
+        } else {
+            showNotFoundPage(req, resp);
+        }
+    }
+
+    private void dispatchPostRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        if (req.getRequestURI().matches(req.getContextPath() + "/web/zirps/?")) {
+            addNewZirp(req, resp);
         } else {
             showNotFoundPage(req, resp);
         }
@@ -46,12 +62,19 @@ public class ZirperServlet extends HttpServlet {
 
         PrintWriter writer = resp.getWriter();
         writer.append("<h1>Zirper</h1>");
+        writer.append(
+            "<form action='" + req.getContextPath() + "/web/zirps' method='post'><div class='row'>"
+            + "<div class='col'><input name='username' type='text' placeholder='Username' /></div>"
+            + "<div class='col'><input name='zirp' type='text' placeholder='Share your thoughts...' /></div>"
+            + "<div class='col'><button class='btn btn-primary' type='submit'>Share</button></div>"
+            + "</div></form><hr />"
+        );
         for (Zirp zirp : zirps) {
             writer.append("<div class='row'>");
             writer.append("<div class='col'>" + formatDate(zirp.getDate()) + "</div>");
             writer.append("<div class='col'><b>" + zirp.getUsername() + "</b></div>");
 
-            writer.append("<div class='col'><i>" + zirp.getZirp().substring(0, 20));
+            writer.append("<div class='col'><i>" + zirp.getZirp().substring(0, Math.min(zirp.getZirp().length(), 20)));
             if (zirp.getZirp().length() >= 20) {
                 writer.append("...&nbsp;<a href='" + req.getContextPath() + "/web/" + zirp.getId() + "'>read more</a>");
             }
@@ -79,6 +102,28 @@ public class ZirperServlet extends HttpServlet {
         writer.append("<h1>Page not found</h1>");
         writer.append("<p>The page you were looking for does not exist.</p><p><a href='" + req.getContextPath()
                 + "/web'>Go back to Zirper</a></p>");
+    }
+
+    private void addNewZirp(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String[] body = req.getReader().lines().collect(Collectors.joining(System.lineSeparator())).split("&");
+
+        String username = "";
+        String zirp = "";
+
+        for (String entry : body) {
+            String[] keyValue = entry.split("=");
+            if (keyValue.length == 2 && keyValue[0].equals("username") && keyValue[1] != null && keyValue[1].length() > 0) {
+                username = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8.toString());
+            }
+            if (keyValue.length == 2 && keyValue[0].equals("zirp") && keyValue[1] != null && keyValue[1].length() > 0) {
+                zirp = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8.toString());
+            }
+        }
+
+        if (username.length() > 0 && zirp.length() > 0) {
+            zirpRepository.save(new Zirp(username, zirp));
+        }
+        resp.sendRedirect(req.getContextPath() + "/web");
     }
 
     private String formatDate(Date date) {
