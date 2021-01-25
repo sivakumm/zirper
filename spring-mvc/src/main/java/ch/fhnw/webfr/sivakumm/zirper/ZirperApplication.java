c545db0bdd59357188c4b750342d0eaa39d5b3e3
Sivakumar, Mithusan <mithusan.sivakumar@zuehlke.com>
2021-01-25 08:37:01 +0100

    initializes spring-mvc project

---
+++ spring-mvc/src/main/java/ch/fhnw/webfr/sivakumm/zirper/ZirperApplication.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webfr.sivakumm.zirper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZirperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZirperApplication.class, args);
+	}
+
+}
---
+++ spring-mvc/src/test/java/ch/fhnw/webfr/sivakumm/zirper/ZirperApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.fhnw.webfr.sivakumm.zirper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZirperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
