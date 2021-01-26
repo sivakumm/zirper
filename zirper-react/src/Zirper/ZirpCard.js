8f554ccc71aa2fbd8449e2563abe257c00f122e6
Sivakumar, Mithusan <mithusan.sivakumar@zuehlke.com>
2021-01-26 14:34:59 +0100

    adds buttons to edit & remove zirps

--- zirper-react/src/Zirper/ZirpCard.js
+++ zirper-react/src/Zirper/ZirpCard.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Card, CardBody, CardText, CardTitle } from 'reactstrap';
+import { Button, Card, CardBody, CardText, CardTitle, Col, Row } from 'reactstrap';
 
 const ZirpCard = ({ zirp }) => {
 	return (
 		<Card className="my-2 mx-auto" style={{ maxWidth: "800px" }}>
 			<CardBody>
-				<CardTitle>@{ zirp.username }</CardTitle>
+				<CardTitle>
+					<Row>
+						<Col>@{ zirp.username }</Col>
+						<Col className="text-right">
+							<Button className="mx-3" color="primary" size="sm"><i class="fas fa-pen"></i></Button>
+							<Button color="danger" size="sm"><i class="fas fa-trash"></i></Button>
+						</Col>
+					</Row>
+				</CardTitle>
 				<CardText><p>{ zirp.zirp }</p></CardText>
 				<footer className="blockquote-footer text-right">
 					<small>{ new Date(zirp.date).toLocaleString() }</small>
