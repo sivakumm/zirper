994ee2bdebcdffdf3d276f274590f3534103f571
Sivakumar, Mithusan <mithusan.sivakumar@zuehlke.com>
2021-01-27 11:42:50 +0100

    renames variable

--- zirper-express/web/dispatcher.js
+++ zirper-express/web/dispatcher.js
@@ -1,15 +1,15 @@
 const dispatcher = require('express').Router();
-const zirp_controller = require('./zirp-controller');
+const zirpController = require('./zirp-controller');
 
 dispatcher.route('/').get((req, res) => res.redirect(`${req.baseUrl}/zirps`));
 
 dispatcher.route('/zirps')
-    .get(zirp_controller.findAll)
-    .post(zirp_controller.create);
+    .get(zirpController.findAll)
+    .post(zirpController.create);
 
 dispatcher.route('/zirps/:id')
-    .get(zirp_controller.findById)
-    .put(zirp_controller.update)
-    .delete(zirp_controller.delete);
+    .get(zirpController.findById)
+    .put(zirpController.update)
+    .delete(zirpController.delete);
 
 module.exports = dispatcher;
