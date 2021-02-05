60b54ddce2747223e3bf6dbbf0e5cba0ada8df34
Mithusan Sivakumar <mithusan.sivakumar@gmail.com>
2021-02-05 11:40:00 +0100

    initializes store

--- zirper-redux/src/index.js
+++ zirper-redux/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const intialState = {
+
+};
+
+const reducer = (state = intialState, action) => {
+  return state;
+};
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={ store }>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
